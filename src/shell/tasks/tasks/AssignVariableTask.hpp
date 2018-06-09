fe1db1405c12f2c46c5f138ba71a7d593532fd1d
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-06-09 20:59:53 +0200

    Add task that assigns variables

---
+++ src/shell/tasks/tasks/AssignVariableTask.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Piotr Żelazko on 09.06.2018.
+//
+
+#ifndef UXP1A_SHELL_ASSIGNVARIABLETASK_HPP
+#define UXP1A_SHELL_ASSIGNVARIABLETASK_HPP
+
+#include "Task.hpp"
+
+namespace shell::tasks {
+    class AssignVariableTask : public Task {
+
+    };
+}
+
+#endif //UXP1A_SHELL_ASSIGNVARIABLETASK_HPP
